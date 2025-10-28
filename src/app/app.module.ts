78960f3943228c30e5fb96ef2793de648275ef77
EnriqueAalvarez795 <julitoalvarezm326@gmail.com>
2025-10-27 21:26:38 -0400

    Angular AR.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArViewComponent } from './components/ar-view/ar-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArViewComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
+
+
---
+++ src/app/components/ar-view/ar-view.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ar-view',
+  templateUrl: './ar-view.component.html',
+  styleUrls: ['./ar-view.component.css']
+})
+export class ArViewComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void { }
+}
